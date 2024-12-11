cbaf85c7944cd0bfecaf901576c469a7d0dffe58
Alex Le Bescond <alex.le_bescond@open-groupe.com>
2024-12-11 11:44:36 +0100

    implementation day 09

--- 2024/src/day-09.ts
+++ 2024/src/day-09.ts
@@ -6,14 +6,66 @@ class Day09 implements DayResultInterface {
 
     // Chemin du fichier d'input permettant de résoudre l'énigme du jour
     private filePath :string = path.join(__dirname, '../data/input_09.txt');
+    private diskMap :number[] = [];
 
     private async initializeList(): Promise<boolean> {
 
         return fs.readFile(this.filePath, 'utf8').then(data => {
-            const lines :string[] = data.split('\n');
+            this.diskMap = data.split('').map(Number);
 
             return true;
         });
+    }
+
+    private compactFile(diskMap :number[]) : number[]
+    {
+        const disk :string[] = [];
+        const compactedFile :number[] = [];
+        let fileId = 0;
+        diskMap.forEach((nbSpace, index) => {
+            if ((index % 2) === 0) {
+                for (let i = 0; i < nbSpace; i++) {
+                    disk.push(fileId.toString());
+                }
+                fileId = fileId + 1;
+                return;
+            }
+
+            for (let i = 0; i < nbSpace; i++) {
+                disk.push('.');
+            }
+        });
+
+        // count the number of free space
+        let spaceToMove = disk.length - 1;
+        const nbBlockFile = disk.filter((space) => space !== '.').length;
+        for (let i = 0; i < nbBlockFile; i++) {
+            if (disk[i] !== '.') {
+                compactedFile.push(parseInt(disk[i]));
+                continue;
+            }
+
+            if (disk[i] === '.') {
+                while(disk[spaceToMove] === '.') {
+                    spaceToMove = spaceToMove - 1;
+                }
+                compactedFile.push(parseInt(disk[spaceToMove]));
+                disk[spaceToMove] = '.';
+            }
+            //this.display(compactedFile);
+        }
+
+        return compactedFile;
+    }
+
+    private display(compactedFile:number[]): void
+    {
+        console.log(compactedFile.join(''));
+    }
+
+    private computeChecksum(compactedFile :number[]): number
+    {
+        return compactedFile.reduce((checksum, id, position) => checksum + (id * position), 0);
     }
 
     public async resultStar1(): Promise<number> {
@@ -21,7 +73,8 @@ class Day09 implements DayResultInterface {
         await this.initializeList();
 
         // Implémentation pour la première étoile du jour 09
-        return 0;
+        const compactedFile = this.compactFile(this.diskMap);
+        return this.computeChecksum(compactedFile);
     }
 
     public async resultStar2(): Promise<number> {
